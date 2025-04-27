00eca2b125582ccf7e73d1f0636fe7f83152864e
Mpass <121707487+ME942082@users.noreply.github.com>
2025-04-28 00:52:54 +0800

    debug

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { logout, register } from "../controllers/UserApi.js";
+import { logout, register } from "../controllers/User.js";
 import passport from "passport";
 
 const router = express.Router();
